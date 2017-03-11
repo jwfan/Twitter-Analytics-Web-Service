8ad6746dcd37ec1aa390e6ba2f5e1999307af5c9
Snow312 <sheepliao312@163.com>
2017-03-11 13:43:26 -0500

    Modify bash

--- query1.autoscaling/autoDeploy.sh
+++ query1.autoscaling/autoDeploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp -jar query1.undertow-0.0.1-SNAPSHOT-jar-with-dependencies.jar lxfree.undertow.service.UndertowServer
+sudo java -cp query1.undertow-0.0.1-SNAPSHOT-jar-with-dependencies.jar lxfree.undertow.service.UndertowServer
