7079723a28ce7e19643b2d6821da10905467ee6d
Snow312 <sheepliao312@163.com>
2017-03-11 16:26:32 -0500

    Add directory in bash

--- query1.autoscaling/autoDeploy.sh
+++ query1.autoscaling/autoDeploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo java -cp query1.undertow-0.0.1-SNAPSHOT-jar-with-dependencies.jar lxfree.undertow.service.UndertowServer
+java -cp /home/ec2-user/query1.undertow-0.0.1-SNAPSHOT-jar-with-dependencies.jar lxfree.undertow.service.UndertowServer
