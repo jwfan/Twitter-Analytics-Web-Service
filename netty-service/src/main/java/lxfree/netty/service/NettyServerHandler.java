d039416474e4a1d97770fa0e7a69ee7be39d6fbe
Jing Xu <kayjade519@gmail.com>
2017-03-11 17:17:04 -0500

    remove print

--- netty-service/src/main/java/lxfree/netty/service/NettyServerHandler.java
+++ netty-service/src/main/java/lxfree/netty/service/NettyServerHandler.java
@@ -47,13 +47,11 @@ public class NettyServerHandler  extends ChannelInboundHandlerAdapter {
 			Matcher matcher1 = pattern1.matcher(requestStr);
 			if (matcher1.find()) {
 				key = requestStr.substring(matcher1.start() + 4, matcher1.end());
-				System.out.println("key: " + key);
 			}
 			Pattern pattern2 = Pattern.compile("message=\\w+");
 			Matcher matcher2 = pattern2.matcher(requestStr);
 			if (matcher2.find()) {
 				message = requestStr.substring(matcher2.start() + 8);
-				System.out.println("cipher: " + message);
 			}
 			
 			/* decipher */
@@ -87,11 +85,8 @@ public class NettyServerHandler  extends ChannelInboundHandlerAdapter {
 				for (int i = Ylength-1; i<Xlength; i++) {
 					sum2+=keyX[i];
 				}
-				System.out.println(sum1);
-				System.out.println(sum2);
 				
 				Z=(sum1+keyY[Ylength-2])%10*10+(sum2+keyY[Ylength-1])%10;
-				System.out.println(Z);
 				/*
 				 * 2.KeyGen step: minikey K = 1 + Z % 25
 				 */
@@ -152,7 +147,6 @@ public class NettyServerHandler  extends ChannelInboundHandlerAdapter {
 				Date date = new Date();
 				SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 				result = TEAMID + "," + TEAM_AWS_ACCOUNT_ID + "\n" + df.format(date) + "\n" + sb.toString() + "\n";
-				System.out.println(result);
 			}
 
 			byte[] CONTENT = result.getBytes();
