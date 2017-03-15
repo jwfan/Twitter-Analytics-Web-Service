0958d27cbf737bb4c874e059a4a929948785729c
Snow312 <sheepliao312@163.com>
2017-03-15 18:04:48 -0400

    Add Test.java

---
+++ query3.mapreduce/src/main/java/lxfree/query3/mapreduce/Test.java
@@ -0,0 +1,57 @@
+package lxfree.query3.mapreduce;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+public class Test {
+	
+	
+
+	public static void main(String[] args) {
+		File f1 = new File("output");
+		File f2 = new File("query3_ref.txt");
+		BufferedReader br = null;
+		Map<String, String> maps = new HashMap<String, String>();
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(f2), StandardCharsets.UTF_8));
+			String line = null;
+			while((line = br.readLine())!=null) {
+				JSONObject jo =  new JSONObject(line);
+				String tid = jo.getString("tweet_id");
+				String score = jo.getString("impactScore");
+				maps.put(tid, score);
+			}
+			br.close();
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(f1), StandardCharsets.UTF_8));
+			while((line = br.readLine())!=null) {
+				String tid = line.split("\t")[0];
+				String score = line.split("\t")[4];
+				if(!maps.containsKey(tid) || !maps.get(tid).equals(score)) {
+					System.out.println(tid);
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(br !=null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+}
