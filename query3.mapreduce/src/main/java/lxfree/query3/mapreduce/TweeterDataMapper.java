4851f6ab19e7657a03a18f298d290380a62c3a53
Flora van <jingweifan11@yahoo.com>
2017-03-15 15:38:03 -0400

    Query3 map&reduce
    Add stop wordlist & calculate impactsocre.

--- query3.mapreduce/src/main/java/lxfree/query3/mapreduce/TweeterDataMapper.java
+++ query3.mapreduce/src/main/java/lxfree/query3/mapreduce/TweeterDataMapper.java
@@ -1,9 +1,11 @@
 package lxfree.query3.mapreduce;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -17,42 +19,66 @@ import java.util.regex.Pattern;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 /**
- * This class is to serve as mapper in MapReduce.
- * List all the data and count as 1
+ * This class is to serve as mapper in MapReduce. List all the data and count as
+ * 1
+ * 
  * @author Ruixue
  *
  */
 public class TweeterDataMapper {
-	
+
 	private final static String SHORTURL_REGEX = "(https?|ftp)://[^\\t\\r\\n /$.?#][^\\t\\r\\n ]*";
 	private final static Pattern NO_LETTER_REGEX = Pattern.compile("['\\-0-9]+");
 	private final static Pattern LETTER_REGEX = Pattern.compile("[A-Za-z(0-9'\\-)?]+");
 	private final static String LANG = "en";
 	private static Map<String, Integer> tIds = new HashMap<String, Integer>();
 	private static Map<String, Integer> countMap = new HashMap<String, Integer>();
-	
+	private static Map<String, Integer> stopWords = new HashMap<String, Integer>();
+
 	public static void main(String[] args) {
 		BufferedReader br = null;
 		PrintWriter out = null;
-//		String fileName = System.getenv("mapreduce_map_input_file");
-//		File file = new File("part-r-00000");
-//		File output = new File("output");
-		try{
-//			br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-//			out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(output), StandardCharsets.UTF_8), true);
+		// String fileName = System.getenv("mapreduce_map_input_file");
+		// File file = new File("part-r-00000");
+		// File output = new File("output");
+		if (stopWords.size() == 0) {
+			InputStream stopfile = TweeterDataMapper.class.getResourceAsStream("/stopwords.txt");
+			BufferedReader stopbr = null;
+			try {
+				stopbr = new BufferedReader(new InputStreamReader(stopfile, StandardCharsets.UTF_8));
+				String l = null;
+				while ((l = stopbr.readLine()) != null) {
+					stopWords.put(l.toLowerCase(), 1);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (stopbr != null) {
+					try {
+						stopbr.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		try {
+			// br = new BufferedReader(new InputStreamReader(new
+			// FileInputStream(file), StandardCharsets.UTF_8));
+			// out = new PrintWriter(new OutputStreamWriter(new
+			// FileOutputStream(output), StandardCharsets.UTF_8), true);
 			br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 			out = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
-			
-			//Load stop words
-			
-			
-			
+
+			// Load stop words
+
 			String line;
-			while((line = br.readLine()) != null) {
-				
+			while ((line = br.readLine()) != null) {
+
 				/*
-				 * 1. Check Malformed Data 
+				 * 1. Check Malformed Data
 				 */
 				JSONObject jo = null;
 				String tid;
@@ -64,131 +90,140 @@ public class TweeterDataMapper {
 				int favorite_count;
 				int retweet_count;
 				int followers_count;
-				try{
-					//Convert to json object
+				try {
+					// Convert to json object
 					jo = new JSONObject(line);
-					
-					//Both id and id_str of the tweet object are missing or empty
-					try{
+
+					// Both id and id_str of the tweet object are missing or
+					// empty
+					try {
 						tid = jo.get("id").toString();
 					} catch (JSONException e1) {
 						try {
 							tid = jo.getString("id_str");
-							if(tid.equals("")){
+							if (tid.equals("")) {
 								continue;
 							}
-						} catch(JSONException e2) {
+						} catch (JSONException e2) {
 							continue;
 						}
 					}
-					
-					//Both id and id_str in user object are missing or empty
+
+					// Both id and id_str in user object are missing or empty
 					JSONObject user = jo.getJSONObject("user");
-					try{
+					try {
 						uid = user.get("id").toString();
 					} catch (JSONException e1) {
 						try {
 							uid = user.getString("id_str");
-							if("".equals(uid)){
+							if ("".equals(uid)) {
 								continue;
 							}
-						} catch(JSONException e2) {
+						} catch (JSONException e2) {
 							continue;
 						}
 					}
-					
-					//created_at field is missing or empty
+
+					// created_at field is missing or empty
 					date = jo.getString("created_at");
-					if("".equals(date)){
+					if ("".equals(date)) {
 						continue;
 					} else {
 						// change date to unix timestamp
 						time = System.currentTimeMillis() / 1000L;
 					}
-					
-					//text field is missing or empty
+
+					// text field is missing or empty
 					text = jo.getString("text");
-					if("".equals(text)) {
+					if ("".equals(text)) {
 						continue;
 					}
-					
-					//lang field is missing or not equal to en
+
+					// lang field is missing or not equal to en
 					lang = jo.getString("lang");
-					if(!LANG.equals(lang)) {
+					if ("".equals(lang)) {
 						continue;
 					}
-					
-					//get favorite, retweet and followers count
+
+					// get favorite, retweet and followers count
 					favorite_count = jo.getInt("favorite_count");
 					retweet_count = jo.getInt("retweet_count");
 					followers_count = user.getInt("followers_count");
-					
-					
-				} catch(JSONException e) {
+
+				} catch (JSONException e) {
 					continue;
 				}
-				
+
 				/*
 				 * 2. Filter out invalid Language of Tweets
 				 */
-					
-				if(!LANG.equals(lang)) {
+
+				if (!LANG.equals(lang)) {
 					continue;
 				}
-					
+
 				/*
 				 * 3. Remove Shortened URLs
 				 */
 				String shortText = text.replaceAll(SHORTURL_REGEX, "");
-				
+
 				/*
 				 * 4. Remove duplicated tweet id
 				 */
-				if(tIds.containsKey(tid)) {
+				if (tIds.containsKey(tid)) {
 					continue;
 				} else {
 					tIds.put(tid, 1);
 				}
-				
-				//split key words and calculate the frequency
+
+				// split key words and calculate the frequency
+				int EWC = 0;
 				StringBuilder keyWords = new StringBuilder();
 				Matcher letterMatcher = LETTER_REGEX.matcher(shortText);
 				String group = "";
 				countMap = new HashMap<String, Integer>();
-				while(letterMatcher.find()) {
+				while (letterMatcher.find()) {
 					group = letterMatcher.group().toLowerCase();
-					if(!NO_LETTER_REGEX.matcher(group).matches()) {
-						keyWords.append(group).append(",");
-						if(countMap.containsKey(group)) {
-							countMap.put(group, countMap.get(group)+1);
-						} else {
-							countMap.put(group, 1);
+					if (!NO_LETTER_REGEX.matcher(group).matches()) {
+						if (!stopWords.containsKey(group)) {
+							keyWords.append(group).append(",");
+							EWC++;
+							if (countMap.containsKey(group)) {
+								countMap.put(group, countMap.get(group) + 1);
+							} else {
+								countMap.put(group, 1);
+							}
 						}
 					}
 				}
-				if(keyWords.length() == 0) {
+				if (keyWords.length() == 0) {
 					continue;
 				}
-				
+
 				// calculate impact score
-				
-				
-				//print out valid data
-				
-				
+				int impact_score = EWC * (favorite_count + retweet_count + followers_count);
+				if (impact_score < 0)
+					impact_score = 0;
+				// print out valid data
+				String wordFreq = "";
+				for (String x : countMap.keySet()) {
+					wordFreq += "\"" + x + "\":" + countMap.get(x) + ",";
+				}
+				out.write(tid + "\t" + uid + "\t" + time + "\t" + impact_score + "\t{"
+						+ wordFreq.substring(0, wordFreq.length() - 1) + "}\n");
+
 			}
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-		finally {
-			if(br!=null) {
+		} finally {
+			if (br != null) {
 				try {
 					br.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-			if(out != null) {
+			if (out != null) {
 				out.close();
 			}
 		}
--- query3.mapreduce/src/main/java/lxfree/query3/mapreduce/TweeterDataReducer.java
+++ query3.mapreduce/src/main/java/lxfree/query3/mapreduce/TweeterDataReducer.java
@@ -1,93 +1,27 @@
 package lxfree.query3.mapreduce;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public final class TweeterDataReducer {
-
+	
 	public static void main(String[] args) {
 		BufferedReader br = null;
 		PrintWriter out = null;
+		String input;
 //		 File file = new File("output");
 //		 File outputfile = new File("output2");
 		try {
-			br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-			out = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
-//			 br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-//			 out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputfile), StandardCharsets.UTF_8), true);
-			String input;
-			String text = null;
-			String hashid = null;
-			String currenthashid = null;
-			String[] keyText = null;
-			String[] hashidArr = null;
-			Map<String, Integer> keyWords = new HashMap<String, Integer>();
-			BigInteger id = BigInteger.ONE;
-			while ((input = br.readLine()) != null) {
-				String[] parts = input.split("\t");
-				text = parts[1];
-				hashid = parts[0];
-				keyText = text.split(",");
-				if (currenthashid != null && currenthashid.equals(hashid)) {
-					for (String a : keyText) {
-						if (keyWords.containsKey(a)) {
-							keyWords.put(a, keyWords.get(a) + 1);
-						} else {
-							keyWords.put(a, 1);
-						}
-					}
-				} else {
-					if (currenthashid != null && !currenthashid.equals(hashid)) {
-						hashidArr = currenthashid.split("#");
-						String output = id + "\t" + hashidArr[0] + "\t" + hashidArr[1] + "\t" + "{";
-						id = id.add(BigInteger.ONE);
-						for (String a : keyWords.keySet()) {
-							output += "\"" + a + "\":" + keyWords.get(a) + ",";
-						}
-						output = output.substring(0, output.length() - 1) + "}";
-						out.write(output + "\n");
-						keyWords = new HashMap<String, Integer>();
-						for (String a : keyText) {
-							if (keyWords.containsKey(a)) {
-								keyWords.put(a, keyWords.get(a) + 1);
-							} else {
-								keyWords.put(a, 1);
-							}
-						}
-						output = "";
-					} else {
-						for (String a : keyText) {
-							if (keyWords.containsKey(a)) {
-								keyWords.put(a, keyWords.get(a) + 1);
-							} else {
-								keyWords.put(a, 1);
-							}
-						}
-					}
-					currenthashid = hashid;
-				}
-			}
-			if (currenthashid != null && currenthashid.equals(hashid)) {
-				hashidArr = currenthashid.split("#");
-				String output = id + "\t" + hashidArr[0] + "\t" + hashidArr[1] + "\t" + "{";
-				for (String a : keyWords.keySet()) {
-					output += "\"" + a + "\":" + keyWords.get(a) + ",";
-				}
-				output = output.substring(0, output.length() - 1) + "}";
-				out.write(output + "\n");
-			}
-		} catch (IOException e) {
+		br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+		out = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8), true);
+		while ((input = br.readLine()) != null) {
+			out.write(input + "\n");
+		}
+		}catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			if (br != null) {
