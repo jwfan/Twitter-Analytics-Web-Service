c5f40459f2249ccbbc8fd3fe15e7b78dd1b716cb
Snow312 <sheepliao312@163.com>
2017-03-16 00:21:00 -0400

    Add query3.backend project

---
+++ query3.backend/src/main/java/lxfree/query2/backend/ConnectionManager.java
@@ -0,0 +1,122 @@
+package lxfree.query2.backend;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class ConnectionManager {
+	//Mysql configuration
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_NAME = "rxl";
+    private static final String URL = "jdbc:mysql://rxldbcc.cmnmvjceyiai.us-east-1.rds.amazonaws.com/" + DB_NAME + "?useSSL=false";
+    private static final String DB_USER = "admin";
+    private static final String DB_PWD = "snowliao11";
+    private static Connection conn;
+    
+    //HBase Configuration
+    private static String zkAddr = "172.31.22.53";
+    private static org.apache.hadoop.hbase.client.Connection hBaseconn;
+    private static final Logger LOGGER = Logger.getRootLogger();
+    
+    //MongoDB Configuration
+    private static MongoDatabase mongoDB;
+    private static final String DNS_ADDRESS = "ec2-52-87-42-136.compute-1.amazonaws.com";
+    private static final String MONGODB_NAME = "comments";
+    
+    /**
+     * Initializes database connection.
+     *
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    private static void initializeConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(JDBC_DRIVER);
+        conn = DriverManager.getConnection(URL, DB_USER, DB_PWD);
+    }
+    
+    /**
+     * Get Mysql connection
+     * @return Mysql Connection
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    	if(conn == null) {
+    		initializeConnection();
+    	}
+    	return conn;
+    }
+    
+    private static void initializeHBaseConnection() throws IOException {
+        // Remember to set correct log level to avoid unnecessary output.
+        LOGGER.setLevel(Level.ALL);
+        if (!zkAddr.matches("\\d+.\\d+.\\d+.\\d+")) {
+            System.out.print("Malformed HBase IP address");
+            System.exit(-1);
+        }
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.master", zkAddr + ":16000");
+        conf.set("hbase.zookeeper.quorum", zkAddr);
+        conf.set("hbase.zookeeper.property.clientport", "2181");
+        hBaseconn = ConnectionFactory.createConnection(conf);
+    }
+    
+    /**
+     * Get Mysql connection
+     * @return HBase Connection
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws IOException 
+     */
+    public static org.apache.hadoop.hbase.client.Connection getHBaseConnection() throws ClassNotFoundException, SQLException, IOException {
+    	if(hBaseconn == null) {
+    		initializeHBaseConnection();
+    	}
+    	return hBaseconn;
+    }
+    
+    /**
+     * Initialize Mongo Database
+     */
+    private static void initializeMongoDB() {
+        try{   
+    		
+            // To connect to mongodb server
+            MongoClient mongoClient = new MongoClient(DNS_ADDRESS , 27017);
+   			
+            // Now connect to your databases
+            mongoDB = mongoClient.getDatabase(MONGODB_NAME);
+            
+            System.out.println("Connect to database successfully");
+         }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         }
+    }
+    
+    /**
+     * Get connection to Mongo DB
+     * @return Mongo Database
+     */
+    public static MongoDatabase getMongoDB() {
+    	if(mongoDB == null) {
+    		initializeMongoDB();
+    	}
+    	return mongoDB;
+    }
+
+}
