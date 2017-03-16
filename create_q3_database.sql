ab8c31832873424558cc70b92c6338d1a4577aa7
Jing Xu <kayjade519@gmail.com>
2017-03-16 00:38:10 -0400

    create q3 database in mysql

---
+++ create_q3_database.sql
@@ -0,0 +1,26 @@
+-- mysql -u <andrewId> --password=CClxfree --local-infile=1 < create_q3_database.sql
+
+-- Step 1 create database
+drop database if exists q3_db;
+create database q3_db;
+use q3_db;
+
+-- Step 2 create twitter table
+drop table if exists `twitter`;
+create table `twitter` (
+	`twitter_id` varchar(20) not null,
+	`user_id` varchar(15) not null,
+	`time_id` varchar(12) not null,
+	`censored_text` LONGTEXT not null,
+	`impact_score` integer default 0 not null,
+	`keywords` LONGTEXT not null,
+	`word_count` integer default 0 not null
+	primary key (twitter_id)
+);
+
+-- Step 3 add data to twitter table
+load data local infile 'part-00000' into table twitter columns terminated by '\t' LINES TERMINATED BY '\n';
+
+-- Step 4 create index
+create index user_index on twitter (user_id);
+create index time_index on twitter (time_id);
