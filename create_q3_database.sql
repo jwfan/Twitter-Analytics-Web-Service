393faf70d685e1e37bf2c071b5eafb736f002f83
Jing Xu <kayjade519@gmail.com>
2017-04-02 03:38:06 -0400

    add utf8

--- create_q3_database.sql
+++ create_q3_database.sql
@@ -1,20 +1,20 @@
--- mysql -u <andrewId> --password=CClxfree --local-infile=1 < create_q3_database.sql
+-- mysql -uroot --password=CClxfreee --local-infile=1 < create_q3_database.sql
 
 -- Step 1 create database
 drop database if exists q3_db;
-create database q3_db;
+create database q3_db default charset utf8 COLLATE utf8_general_ci;
 use q3_db;
 
 -- Step 2 create twitter table
-drop table if exists `q3_table` default charset utf8 COLLATE utf8_general_ci;
-create table `q3_table` (
-	`twitter_id` varchar(20) not null,
-	`user_id` varchar(19) not null,
-	`time_stamp` varchar(13) not null,
-	`censored_text` LONGTEXT not null,
-	`impact_score` integer default 0 not null,
-	`keywords` LONGTEXT not null,
-	`word_count` integer default 0 not null
+drop table if exists q3_table;
+create table q3_table (
+	twitter_id varchar(20) not null,
+	user_id varchar(19) not null,
+	time_stamp varchar(13) not null,
+	censored_text LONGTEXT not null,
+	impact_score integer default 0 not null,
+	keywords LONGTEXT not null,
+	word_count integer default 0 not null,
 	primary key (twitter_id)
 );
 
