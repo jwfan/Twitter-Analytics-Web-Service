69421c69a4017fcae503bf436a4a8d6c1db1b8f8
Jing Xu <kayjade519@gmail.com>
2017-04-02 13:47:16 -0400

    mysql config use utf8mb4

--- create_q3_database.sql
+++ create_q3_database.sql
@@ -2,11 +2,13 @@
 
 -- Step 1 create database
 drop database if exists q3_db;
-create database q3_db default charset utf8 COLLATE utf8_general_ci;
+create database q3_db;
+ALTER DATABASE q3_db CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 use q3_db;
 
 -- Step 2 create twitter table
 drop table if exists q3_table;
+
 create table q3_table (
 	twitter_id varchar(20) not null,
 	user_id varchar(19) not null,
@@ -17,6 +19,10 @@ create table q3_table (
 	word_count integer default 0 not null,
 	primary key (twitter_id)
 );
+
+ALTER TABLE q3_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE q3_table CHANGE censored_text censored_text LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE q3_table CHANGE keywords keywords LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Step 3 add data to twitter table
 load data local infile 'part-00000' into table q3_table columns terminated by '\t' LINES TERMINATED BY '\n';
