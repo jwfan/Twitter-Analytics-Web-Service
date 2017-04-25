9b4a184a3fc659f4fc9b5bc702c04310271202a8
Jing Xu <kayjade519@gmail.com>
2017-04-25 13:08:09 -0400

    modify ioThread

--- lxfree.p3.mysql/src/main/java/p3/mysql/UndertowServer.java
+++ lxfree.p3.mysql/src/main/java/p3/mysql/UndertowServer.java
@@ -14,12 +14,15 @@ import query2.Query2Handler;
  */
 public class UndertowServer {
 
+	private static int ioThread=20;
+	private static int workerThread=ioThread*8;
+	
 	public static void main(String[] args){
 		Undertow server=Undertow.builder()
-				.addHttpListener(80, "localhost")
-				.setIoThreads(8)
-				.setWorkerThreads(64)
-				.setWorkerOption(Options.WORKER_TASK_MAX_THREADS, 64)
+				.addHttpListener(80, "0.0.0.0")
+				.setIoThreads(ioThread)
+				.setWorkerThreads(workerThread)
+				.setWorkerOption(Options.WORKER_TASK_MAX_THREADS, workerThread)
 				.setHandler(Handlers.path()	// path routing
 						.addPrefixPath("/q1", new Query1Handler())
 						.addPrefixPath("/q2", new Query2Handler())
